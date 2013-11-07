9fbfdbc87b2149312eeb9c409f7ab7647481377b
tpetr <tpetr@hubspot.com>
2013-11-06 22:26:38 -0500

    remove extra healthcheck stuff

--- BaragonBase/src/main/java/com/hubspot/baragon/healthchecks/HealthCheckException.java
+++
@@ -1,9 +0,0 @@
-package com.hubspot.baragon.healthchecks;
-
-import java.util.Map;
-
-public class HealthCheckException extends RuntimeException {
-  public HealthCheckException(final Map<String, String> serverErrors) {
-    // TODO: make better
-  }
-}
