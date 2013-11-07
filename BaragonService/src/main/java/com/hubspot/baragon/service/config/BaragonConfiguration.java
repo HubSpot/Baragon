b9ba2fbf347beeab161cf12f9841f63744a9144a
tpetr <tpetr@hubspot.com>
2013-11-06 19:21:11 -0500

    ignore unknown in config

--- BaragonService/src/main/java/com/hubspot/baragon/service/config/BaragonConfiguration.java
+++ BaragonService/src/main/java/com/hubspot/baragon/service/config/BaragonConfiguration.java
@@ -1,9 +1,11 @@
 package com.hubspot.baragon.service.config;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.hubspot.baragon.config.ZooKeeperConfiguration;
 import io.dropwizard.Configuration;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BaragonConfiguration extends Configuration {
   @JsonProperty("zookeeper")
   private ZooKeeperConfiguration zooKeeperConfiguration;
