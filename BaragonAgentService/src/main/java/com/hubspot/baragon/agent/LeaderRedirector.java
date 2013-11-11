5ef3a47be0fe3efa1cee484184e68316840b8697
tpetr <tpetr@hubspot.com>
2013-11-11 15:26:42 -0500

    make redirector actually work

--- BaragonAgentService/src/main/java/com/hubspot/baragon/agent/LeaderRedirector.java
+++ BaragonAgentService/src/main/java/com/hubspot/baragon/agent/LeaderRedirector.java
@@ -3,30 +3,31 @@ package com.hubspot.baragon.agent;
 import com.google.common.base.Throwables;
 import com.google.inject.Inject;
 import com.google.inject.servlet.RequestScoped;
-import com.sun.jersey.api.core.HttpRequestContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.curator.framework.recipes.leader.LeaderLatch;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Context;
 import java.net.URI;
 
 @RequestScoped
 public class LeaderRedirector {
+  private static final Log LOG = LogFactory.getLog(LeaderRedirector.class);
   private final LeaderLatch leaderLatch;
-
-  @Context
-  private HttpRequestContext request;
+  private final HttpServletRequest request;
 
   @Inject
-  public LeaderRedirector(LeaderLatch leaderLatch) {
+  public LeaderRedirector(LeaderLatch leaderLatch, HttpServletRequest request) {
     this.leaderLatch = leaderLatch;
+    this.request = request;
   }
 
   public void redirectToLeader() {
     if (!leaderLatch.hasLeadership()) {
       try {
         throw new WebApplicationException(javax.ws.rs.core.Response
-            .temporaryRedirect(URI.create(String.format("http://%s%s", leaderLatch.getLeader().getId(), request.getAbsolutePath().getPath())))
+            .temporaryRedirect(URI.create(String.format("http://%s%s%s%s", leaderLatch.getLeader().getId(), request.getContextPath(), request.getServletPath(), request.getPathInfo())))
             .build());
       } catch (Exception e) {
         throw Throwables.propagate(e);
