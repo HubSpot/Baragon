d5e5500773ffbe9806007d6d98ccd360539d96b0
Ian Marlier <imarlier@hubspot.com>
2013-12-26 14:50:59 -0500

    Commit against the Github repo, instead of GH:E

--- BaragonBase/src/main/java/com/hubspot/baragon/models/ServiceInfo.java
+++ BaragonBase/src/main/java/com/hubspot/baragon/models/ServiceInfo.java
@@ -3,10 +3,14 @@ package com.hubspot.baragon.models;
 import com.fasterxml.jackson.annotation.*;
 import com.google.common.base.Objects;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import java.util.Collections;
 import java.util.List;
 
 public class ServiceInfo {
+  private static final Log LOG = LogFactory.getLog(ServiceInfo.class);
   private final String name;
   private final String id;
   private final String contactEmail;
@@ -14,12 +18,14 @@ public class ServiceInfo {
   private final List<String> extraConfigs;
   private final String healthCheck;
   private final List<String> lbs;
+  private final String rewriteAppRootTo;
   
   public ServiceInfo(@JsonProperty("name") String name, @JsonProperty("id") String id,
                      @JsonProperty("contactEmail") String contactEmail, @JsonProperty("route") String route,
                      @JsonProperty("extraConfigs") List<String> extraConfigs,
                      @JsonProperty("healthCheck") String healthCheck,
-                     @JsonProperty("lbs") List<String> lbs) {
+                     @JsonProperty("lbs") List<String> lbs,
+                     @JsonProperty("rewriteAppRootTo") String rewriteAppRootTo) {
     this.name = name;
     this.id = id;
     this.contactEmail = contactEmail;
@@ -27,6 +33,17 @@ public class ServiceInfo {
     this.extraConfigs = Objects.firstNonNull(extraConfigs, Collections.<String>emptyList());
     this.healthCheck = healthCheck;
     this.lbs = lbs;
+    if (rewriteAppRootTo != null) {
+      if (route.substring(route.length() - 1) == "/" && rewriteAppRootTo.substring(rewriteAppRootTo.length() - 1) != "/") {
+        LOG.error(String.format("Provided appRoot %s is absolute, and rewriteAppRootTo %s is not.  This will result in a rewrite of %sresource to %sresource",
+                                    route, rewriteAppRootTo, route, rewriteAppRootTo));
+      } else if (route.substring(route.length() - 1) != "/" && rewriteAppRootTo.substring(rewriteAppRootTo.length() - 1) == "/") {
+        LOG.error(String.format("Provided appRoot %s is not absolute, and rewriteAppRootTo %s is.  This will result in a rewrite of %sresource to %s resource",
+                                    route, rewriteAppRootTo, route, rewriteAppRootTo));
+      } else {
+        this.rewriteAppRootTo = rewriteAppRootTo;
+      }
+    }
   }
   
   public String getName() {
@@ -55,5 +72,9 @@ public class ServiceInfo {
 
   public List<String> getLbs() {
     return lbs;
+  }
+
+  public String getRewriteAppRootTo() {
+    return rewriteAppRootTo;
   }
 }
