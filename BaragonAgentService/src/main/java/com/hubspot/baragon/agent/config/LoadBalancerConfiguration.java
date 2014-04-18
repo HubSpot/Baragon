ec35ca27080ad6d4d6b73b367e31c1865f619558
tpetr <tpetr@hubspot.com>
2014-04-18 15:29:46 -0400

    clean up LoadBalancerConfiguration

--- BaragonAgentService/src/main/java/com/hubspot/baragon/agent/config/LoadBalancerConfiguration.java
+++ BaragonAgentService/src/main/java/com/hubspot/baragon/agent/config/LoadBalancerConfiguration.java
@@ -17,18 +17,6 @@ public class LoadBalancerConfiguration {
   @NotNull
   private String reloadConfigCommand;
 
-  @NotNull
-  private String proxyTemplate;
-
-  @NotNull
-  private String upstreamTemplate;
-
-  @NotNull
-  private Boolean rollbackConfigsIfInvalid = false;
-
-  @NotNull
-  private Boolean alwaysApplyConfigs = false;
-
   @Min(0)
   private int commandTimeoutMs = 10000;
 
@@ -36,68 +24,32 @@ public class LoadBalancerConfiguration {
     return name;
   }
 
-  public String getRootPath() {
-    return rootPath;
-  }
-
   public void setName(String name) {
     this.name = name;
+  }
+
+  public String getRootPath() {
+    return rootPath;
   }
 
   public void setRootPath(String rootPath) {
     this.rootPath = rootPath;
   }
 
-  public void setCheckConfigCommand(String checkConfigCommand) {
-    this.checkConfigCommand = checkConfigCommand;
-  }
-
   public String getCheckConfigCommand() {
     return checkConfigCommand;
   }
 
-  public void setReloadConfigCommand(String reloadConfigCommand) {
-    this.reloadConfigCommand = reloadConfigCommand;
+  public void setCheckConfigCommand(String checkConfigCommand) {
+    this.checkConfigCommand = checkConfigCommand;
   }
 
   public String getReloadConfigCommand() {
     return reloadConfigCommand;
   }
 
-  public void setProxyTemplate(String proxyTemplate) {
-    this.proxyTemplate = proxyTemplate;
-  }
-
-  public String getProxyTemplate() {
-    return proxyTemplate;
-  }
-
-  public void setUpstreamTemplatePath(String upstreamTemplate) {
-    this.upstreamTemplate = upstreamTemplate;
-  }
-
-  public String getUpstreamTemplate() {
-    return upstreamTemplate;
-  }
-
-  public void setRollbackConfigsIfInvalid(Boolean rollbackConfigsIfInvalid) {
-    this.rollbackConfigsIfInvalid = rollbackConfigsIfInvalid;
-  }
-
-  public Boolean getRollbackConfigsIfInvalid() {
-    return rollbackConfigsIfInvalid;
-  }
-
-  public Boolean getAlwaysApplyConfigs() {
-    return alwaysApplyConfigs;
-  }
-
-  public void setAlwaysApplyConfigs(Boolean alwaysApplyConfigs) {
-    this.alwaysApplyConfigs = alwaysApplyConfigs;
-  }
-
-  public void setUpstreamTemplate(String upstreamTemplate) {
-    this.upstreamTemplate = upstreamTemplate;
+  public void setReloadConfigCommand(String reloadConfigCommand) {
+    this.reloadConfigCommand = reloadConfigCommand;
   }
 
   public int getCommandTimeoutMs() {
