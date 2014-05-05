678910a511f70ea5805422d25c3e8e0bd9f59f69
tpetr <tpetr@hubspot.com>
2014-05-05 11:53:32 -0400

    log received BaragonRequest

--- BaragonService/src/main/java/com/hubspot/baragon/service/resources/RequestResource.java
+++ BaragonService/src/main/java/com/hubspot/baragon/service/resources/RequestResource.java
@@ -8,6 +8,8 @@ import com.hubspot.baragon.managers.RequestManager;
 import com.hubspot.baragon.models.BaragonRequest;
 import com.hubspot.baragon.models.BaragonResponse;
 import com.hubspot.baragon.service.worker.BaragonRequestWorker;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import javax.validation.Valid;
 import javax.ws.rs.*;
@@ -18,6 +20,8 @@ import javax.ws.rs.core.Response;
 @Consumes({MediaType.APPLICATION_JSON})
 @Produces(MediaType.APPLICATION_JSON)
 public class RequestResource {
+  private static final Log LOG = LogFactory.getLog(RequestResource.class);
+
   private final RequestManager manager;
 
   @Inject
@@ -34,12 +38,15 @@ public class RequestResource {
   @POST
   public BaragonResponse enqueueRequest(@Valid BaragonRequest request) {
     try {
+      LOG.info(String.format("Received request: %s", request));
       return manager.enqueueRequest(request);
     } catch (BasePathConflictException e) {
+      LOG.warn(String.format("Base path conflict for request %s", request.getLoadBalancerRequestId()), e);
       throw new WebApplicationException(Response.status(Response.Status.CONFLICT)
           .entity(BaragonResponse.failure(request.getLoadBalancerRequestId(), "Another service already exists with this path."))
           .build());
     } catch (MissingLoadBalancerGroupException e) {
+      LOG.warn(String.format("Missing load balancer(s) for request %s", request.getLoadBalancerRequestId()), e);
       throw new WebApplicationException(Response.status(Response.Status.BAD_REQUEST)
           .entity(BaragonResponse.failure(request.getLoadBalancerRequestId(), "One or more load balancer groups do not exist"))
           .build());
