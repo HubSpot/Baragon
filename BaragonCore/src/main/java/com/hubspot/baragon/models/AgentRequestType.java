4d4b0bc80e9d93223fa9d251fc2c72d823e1dc4d
tpetr <tpetr@hubspot.com>
2014-06-10 11:51:11 -0400

    JsonCreators for enums

--- BaragonCore/src/main/java/com/hubspot/baragon/models/AgentRequestType.java
+++ BaragonCore/src/main/java/com/hubspot/baragon/models/AgentRequestType.java
@@ -1,7 +1,18 @@
 package com.hubspot.baragon.models;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum AgentRequestType {
   APPLY,
   REVERT,
-  CANCEL
+  CANCEL;
+
+  @JsonCreator
+  public static AgentRequestType fromString(String value) {
+    if (value == null || value.equals("")) {
+      return null;
+    } else {
+      return valueOf(value.toUpperCase());
+    }
+  }
 }
--- BaragonCore/src/main/java/com/hubspot/baragon/models/BaragonRequestState.java
+++ BaragonCore/src/main/java/com/hubspot/baragon/models/BaragonRequestState.java
@@ -1,5 +1,16 @@
 package com.hubspot.baragon.models;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum BaragonRequestState {
-  UNKNOWN, FAILED, WAITING, SUCCESS, CANCELING, CANCELED
+  UNKNOWN, FAILED, WAITING, SUCCESS, CANCELING, CANCELED;
+
+  @JsonCreator
+  public static BaragonRequestState fromString(String value) {
+    if (value == null || value.equals("")) {
+      return null;
+    } else {
+      return valueOf(value.toUpperCase());
+    }
+  }
 }
