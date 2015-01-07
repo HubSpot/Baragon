f330712a82abc16239c49d1eeede9a824376dd8f
Eric Herot <eric.github@herot.com>
2015-01-07 18:32:08 -0500

    Make the agent port an attribute

--- cookbook/attributes/agent.rb
+++ cookbook/attributes/agent.rb
@@ -1,3 +1,4 @@
 default[:baragon][:group_name] = 'default'
 default[:baragon][:proxy_conf_dir] = '/tmp'
 default[:baragon][:upstream_conf_dir] = node[:baragon][:proxy_conf_dir]
+default[:baragon][:agent_port] = 8882
