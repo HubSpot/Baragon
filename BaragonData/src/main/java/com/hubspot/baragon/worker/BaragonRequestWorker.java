b4ef1e5bd8f0cefe438e2ed529ff09810a36df4a
tpetr <tpetr@hubspot.com>
2015-01-08 16:56:46 -0500

    simplify state transitions

--- BaragonData/src/main/java/com/hubspot/baragon/worker/BaragonRequestWorker.java
+++ BaragonData/src/main/java/com/hubspot/baragon/worker/BaragonRequestWorker.java
@@ -1,6 +1,8 @@
 package com.hubspot.baragon.worker;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -14,9 +16,12 @@ import com.google.inject.name.Named;
 import com.hubspot.baragon.BaragonDataModule;
 import com.hubspot.baragon.managers.AgentManager;
 import com.hubspot.baragon.managers.RequestManager;
+import com.hubspot.baragon.models.AgentRequestType;
+import com.hubspot.baragon.models.AgentResponse;
 import com.hubspot.baragon.models.BaragonRequest;
 import com.hubspot.baragon.models.InternalRequestStates;
 import com.hubspot.baragon.models.QueuedRequestId;
+import com.hubspot.baragon.utils.JavaUtils;
 
 public class BaragonRequestWorker implements Runnable {
   private static final Logger LOG = LoggerFactory.getLogger(BaragonRequestWorker.class);
@@ -34,15 +39,100 @@ public class BaragonRequestWorker implements Runnable {
     this.workerLastStartAt = workerLastStartAt;
   }
 
-  public void handleRequest(QueuedRequestId queuedRequestId) {
+  private String buildResponseString(Map<String, Collection<AgentResponse>> agentResponses, AgentRequestType requestType) {
+    if (agentResponses.containsKey(requestType.name())) {
+      return JavaUtils.COMMA_JOINER.join(agentResponses.get(requestType.name()));
+    } else {
+      return "no responses";
+    }
+  }
+
+  private InternalRequestStates handleCheckRevertResponse(BaragonRequest request, InternalRequestStates currentState) {
+    final Map<String, Collection<AgentResponse>> agentResponses;
+
+    switch (agentManager.getRequestsStatus(request, currentState.getRequestType())) {
+      case FAILURE:
+        agentResponses = agentManager.getAgentResponses(request.getLoadBalancerRequestId());
+        requestManager.setRequestMessage(request.getLoadBalancerRequestId(), String.format("Apply failed (%s), %s failed (%s)", buildResponseString(agentResponses, AgentRequestType.APPLY), currentState.getRequestType().name(), buildResponseString(agentResponses, currentState.getRequestType())));
+        return currentState.getRequestType().getFailureState();
+      case SUCCESS:
+        agentResponses = agentManager.getAgentResponses(request.getLoadBalancerRequestId());
+        requestManager.setRequestMessage(request.getLoadBalancerRequestId(), String.format("Apply failed (%s), %s OK.", buildResponseString(agentResponses, AgentRequestType.APPLY), currentState.getRequestType().name()));
+        return currentState.getRequestType().getSuccessState();
+      case RETRY:
+        return currentState.getRequestType().getRetryState();
+      default:
+        return currentState.getRequestType().getWaitingState();
+    }
+  }
+
+  private InternalRequestStates handleState(InternalRequestStates currentState, BaragonRequest request) {
+    switch (currentState) {
+      case PENDING:
+        final Map<String, String> conflicts = requestManager.getBasePathConflicts(request);
+
+        if (!conflicts.isEmpty()) {
+          requestManager.setRequestMessage(request.getLoadBalancerRequestId(), String.format("Invalid request due to base path conflicts: %s", conflicts));
+          return InternalRequestStates.INVALID_REQUEST_NOOP;
+        }
+
+        final Set<String> missingGroups = requestManager.getMissingLoadBalancerGroups(request);
+
+        if (!missingGroups.isEmpty()) {
+          requestManager.setRequestMessage(request.getLoadBalancerRequestId(), String.format("Invalid request due to non-existent load balancer groups: %s", missingGroups));
+          return InternalRequestStates.INVALID_REQUEST_NOOP;
+        }
+
+        requestManager.lockBasePaths(request);
+
+        return InternalRequestStates.SEND_APPLY_REQUESTS;
+
+      case CHECK_APPLY_RESPONSES:
+        switch (agentManager.getRequestsStatus(request, currentState.getRequestType())) {
+          case FAILURE:
+            final Map<String, Collection<AgentResponse>> agentResponses = agentManager.getAgentResponses(request.getLoadBalancerRequestId());
+            requestManager.setRequestMessage(request.getLoadBalancerRequestId(), String.format("Apply failed (%s), reverting...", buildResponseString(agentResponses, currentState.getRequestType())));
+            return InternalRequestStates.FAILED_SEND_REVERT_REQUESTS;
+          case SUCCESS:
+            try {
+              requestManager.commitRequest(request);
+              return InternalRequestStates.COMPLETED;
+            } catch (Exception e) {
+              LOG.warn(String.format("Request %s was successful, but failed to commit!", request.getLoadBalancerRequestId()), e);
+              return InternalRequestStates.FAILED_SEND_REVERT_REQUESTS;
+            }
+          case RETRY:
+            return InternalRequestStates.SEND_APPLY_REQUESTS;
+          default:
+            return InternalRequestStates.CHECK_APPLY_RESPONSES;
+        }
+
+      case SEND_APPLY_REQUESTS:
+      case FAILED_SEND_REVERT_REQUESTS:
+      case CANCELLED_SEND_REVERT_REQUESTS:
+        agentManager.sendRequests(request, currentState.getRequestType());
+        return currentState.getRequestType().getWaitingState();
+
+      case FAILED_CHECK_REVERT_RESPONSES:
+      case CANCELLED_CHECK_REVERT_RESPONSES:
+        return handleCheckRevertResponse(request, currentState);
+
+      default:
+        return currentState;
+    }
+  }
+
+  public void handleQueuedRequest(QueuedRequestId queuedRequestId) {
     final String requestId = queuedRequestId.getRequestId();
 
-    final Optional<InternalRequestStates> maybeStatus = requestManager.getRequestState(requestId);
+    final Optional<InternalRequestStates> maybeState = requestManager.getRequestState(requestId);
 
-    if (!maybeStatus.isPresent()) {
+    if (!maybeState.isPresent()) {
       LOG.warn(String.format("%s does not have a request status!", requestId));
       return;
     }
+
+    final InternalRequestStates currentState = maybeState.get();
 
     final Optional<BaragonRequest> maybeRequest = requestManager.getRequest(requestId);
 
@@ -51,14 +141,14 @@ public class BaragonRequestWorker implements Runnable {
       return;
     }
 
-    final Optional<InternalRequestStates> maybeNewStatus = maybeStatus.get().handle(maybeRequest.get(), agentManager, requestManager);
+    final InternalRequestStates newState = handleState(currentState, maybeRequest.get());
 
-    if (maybeNewStatus.isPresent()) {
-      LOG.info(String.format("%s: %s --> %s", requestId, maybeStatus.get(), maybeNewStatus.get()));
-      requestManager.setRequestState(requestId, maybeNewStatus.get());
+    if (newState != currentState) {
+      LOG.info(String.format("%s: %s --> %s", requestId, currentState, newState));
+      requestManager.setRequestState(requestId, newState);
     }
 
-    if (maybeNewStatus.or(maybeStatus.get()).isRemovable()) {
+    if (newState.isRemovable()) {
       requestManager.removeQueuedRequest(queuedRequestId);
     }
   }
@@ -77,7 +167,7 @@ public class BaragonRequestWorker implements Runnable {
         for (QueuedRequestId queuedRequestId : queuedRequestIds) {
           if (!handledServices.contains(queuedRequestId.getServiceId())) {
             synchronized (BaragonRequestWorker.class) {
-              handleRequest(queuedRequestId);
+              handleQueuedRequest(queuedRequestId);
             }
             handledServices.add(queuedRequestId.getServiceId());
           }
