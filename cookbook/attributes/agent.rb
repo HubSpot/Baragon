0232ee5c33ab83a90ae90b9f53e940c67ad95c18
Eric Herot <eric.github@herot.com>
2015-01-09 15:10:10 -0500

    Set Baragon domain by way of attribute

--- cookbook/attributes/agent.rb
+++ cookbook/attributes/agent.rb
@@ -3,3 +3,4 @@ default[:baragon][:proxy_conf_dir] = '/tmp'
 default[:baragon][:upstream_conf_dir] = node[:baragon][:proxy_conf_dir]
 default[:baragon][:agent_port] = 8882
 default[:baragon][:agent_app_context_path] = '/baragon-agent/v2'
+default[:baragon][:domain] = 'vagrant.baragon.biz'
