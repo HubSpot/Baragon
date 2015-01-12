6d0273cd611f30f46ec87a70e46fa2ffa6e4934c
Eric Herot <eric.github@herot.com>
2015-01-12 15:11:31 -0500

    Attributize log locations

--- cookbook/attributes/agent.rb
+++ cookbook/attributes/agent.rb
@@ -4,3 +4,4 @@ default[:baragon][:upstream_conf_dir] = node[:baragon][:proxy_conf_dir]
 default[:baragon][:agent_port] = 8882
 default[:baragon][:agent_app_context_path] = '/baragon-agent/v2'
 default[:baragon][:domain] = 'vagrant.baragon.biz'
+default[:baragon][:agent_log] = '/var/log/baragon/baragon_agent.log'
--- cookbook/attributes/service.rb
+++ cookbook/attributes/service.rb
@@ -1,2 +1,3 @@
 default[:baragon][:server_port] = 8088
 default[:baragon][:server_app_context_path] = '/baragon/v2'
+default[:baragon][:service_log] = '/var/log/baragon/baragon_service.log'
