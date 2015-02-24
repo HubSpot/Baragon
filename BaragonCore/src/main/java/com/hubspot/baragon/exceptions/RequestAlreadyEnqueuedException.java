bbbd23f7afcf453d172db7310e865b771d97fd72
Stephen Salinas <ssalinas@hubspot.com>
2015-02-24 11:10:26 -0500

    Return current response when request already enqueued and matches

--- BaragonCore/src/main/java/com/hubspot/baragon/exceptions/RequestAlreadyEnqueuedException.java
+++ BaragonCore/src/main/java/com/hubspot/baragon/exceptions/RequestAlreadyEnqueuedException.java
@@ -6,7 +6,8 @@ public class RequestAlreadyEnqueuedException extends Exception {
   private final String requestId;
   private final BaragonResponse response;
 
-  public RequestAlreadyEnqueuedException(String requestId, BaragonResponse response) {
+  public RequestAlreadyEnqueuedException(String requestId, BaragonResponse response, String message) {
+    super(message);
     this.requestId = requestId;
     this.response = response;
   }
