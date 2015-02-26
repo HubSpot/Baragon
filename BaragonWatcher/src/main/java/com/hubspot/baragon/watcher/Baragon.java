9924a5ad215ea0b1e05371058012e8bc8dea16cf
Jonathan Haber <jhaber@hubspot.com>
2015-02-26 16:55:14 -0500

    Add Watcher module

---
+++ BaragonWatcher/src/main/java/com/hubspot/baragon/watcher/Baragon.java
@@ -0,0 +1,13 @@
+package com.hubspot.baragon.watcher;
+
+import com.google.inject.BindingAnnotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.PARAMETER, ElementType.METHOD })
+@BindingAnnotation
+public @interface Baragon {}
