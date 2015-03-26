5a2d402fe42dffcacb28dd6d677226c6c4df76ea
Stephen Salinas <ssalinas@hubspot.com>
2015-03-26 14:11:05 -0400

    move if statement for better logging output

--- BaragonAgentService/src/main/java/com/hubspot/baragon/agent/lbs/BootstrapFileChecker.java
+++ BaragonAgentService/src/main/java/com/hubspot/baragon/agent/lbs/BootstrapFileChecker.java
@@ -16,17 +16,14 @@ import com.hubspot.baragon.models.ServiceContext;
 public class BootstrapFileChecker implements Callable<Optional<Pair<ServiceContext, Collection<BaragonConfigFile>>>> {
   private static final Logger LOG = LoggerFactory.getLogger(BootstrapFileChecker.class);
 
-  private final LoadBalancerConfiguration loadBalancerConfiguration;
   private final FilesystemConfigHelper configHelper;
   private final BaragonServiceState serviceState;
   private final long now;
 
-  public BootstrapFileChecker(LoadBalancerConfiguration loadBalancerConfiguration,
-                              FilesystemConfigHelper configHelper,
+  public BootstrapFileChecker(FilesystemConfigHelper configHelper,
                               BaragonServiceState serviceState,
                               long now) {
 
-    this.loadBalancerConfiguration = loadBalancerConfiguration;
     this.configHelper = configHelper;
     this.serviceState = serviceState;
     this.now = now;
@@ -34,17 +31,15 @@ public class BootstrapFileChecker implements Callable<Optional<Pair<ServiceConte
 
   @Override
   public Optional<Pair<ServiceContext, Collection<BaragonConfigFile>>> call() {
-    if (!(serviceState.getService().getLoadBalancerGroups() == null) && serviceState.getService().getLoadBalancerGroups().contains(loadBalancerConfiguration.getName())) {
-      try {
-        ServiceContext context = new ServiceContext(serviceState.getService(), serviceState.getUpstreams(), now, true);
-        Optional<Collection<BaragonConfigFile>> maybeConfigsToApply = configHelper.configsToApply(context);
-        if (maybeConfigsToApply.isPresent()) {
-          Pair<ServiceContext, Collection<BaragonConfigFile>> configMap = new ImmutablePair<>(context, maybeConfigsToApply.get());
-          return Optional.of(configMap);
-        }
-      } catch (Exception e) {
-        LOG.error(String.format("Caught exception while finding config for %s", serviceState.getService()), e);
+    try {
+      ServiceContext context = new ServiceContext(serviceState.getService(), serviceState.getUpstreams(), now, true);
+      Optional<Collection<BaragonConfigFile>> maybeConfigsToApply = configHelper.configsToApply(context);
+      if (maybeConfigsToApply.isPresent()) {
+        Pair<ServiceContext, Collection<BaragonConfigFile>> configMap = new ImmutablePair<>(context, maybeConfigsToApply.get());
+        return Optional.of(configMap);
       }
+    } catch (Exception e) {
+      LOG.error(String.format("Caught exception while finding config for %s", serviceState.getService()), e);
     }
     LOG.info(String.format("Don't need to apply %s", serviceState.getService().getServiceId()));
     return Optional.absent();
