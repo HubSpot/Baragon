7b4305bde2abf4df4ad7f97f913bfc2a22cef08a
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-04-13 14:09:52 -0400

    Update tests to look for the namespaced agent files/services

--- cookbook/test/integration/localzk/serverspec/agent_spec.rb
+++ cookbook/test/integration/localzk/serverspec/agent_spec.rb
@@ -11,11 +11,11 @@ describe 'Services' do
     it { is_expected.to be_listening.with('tcp6') }
   end
   it 'has a running and enabled baragon-agent service' do
-    expect(service('baragon-agent')).to be_enabled
-    expect(service('baragon-agent')).to be_running
+    expect(service('baragon-agent-default')).to be_enabled
+    expect(service('baragon-agent-default')).to be_running
   end
   it 'has a running and enabled baragon-server service' do
-    expect(service('baragon-server')).to be_enabled
-    expect(service('baragon-server')).to be_running
+    expect(service('baragon-server-default')).to be_enabled
+    expect(service('baragon-server-default')).to be_running
   end
 end
