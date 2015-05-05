09b0b015e4ca4e4dc20ee15a1e05f40519385b5a
Eric Herot <eric.github@herot.com>
2015-05-05 11:27:34 -0400

    Use Baragon 0.1.7 git tag instead of arbitrary ref from master

--- cookbook/attributes/default.rb
+++ cookbook/attributes/default.rb
@@ -2,5 +2,5 @@ default[:baragon][:version] = '0.1.7'
 default[:baragon][:zk_namespace] = 'baragon'
 default[:baragon][:zk_port] = 2181
 default[:baragon][:user] = 'baragon'
-default[:baragon][:git_ref] = 'f72e31d62ab098bed6c145ec8e9b5763f22ea833'
+default[:baragon][:git_ref] = 'Baragon-0.1.7'
 default[:baragon][:install_type] = 'package'
