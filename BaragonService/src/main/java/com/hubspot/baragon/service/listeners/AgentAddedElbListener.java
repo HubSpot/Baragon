d47de81a9f0dadc63274c59f697fdc9313451eca
Stephen Salinas <ssalinas@hubspot.com>
2015-05-20 14:49:40 -0400

    more checks before registering new instance

--- BaragonService/src/main/java/com/hubspot/baragon/service/listeners/AgentAddedElbListener.java
+++ BaragonService/src/main/java/com/hubspot/baragon/service/listeners/AgentAddedElbListener.java
@@ -4,7 +4,9 @@ import java.util.Arrays;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.services.elasticloadbalancing.AmazonElasticLoadBalancingClient;
+import com.amazonaws.services.elasticloadbalancing.model.DescribeLoadBalancersRequest;
 import com.amazonaws.services.elasticloadbalancing.model.Instance;
+import com.amazonaws.services.elasticloadbalancing.model.LoadBalancerDescription;
 import com.amazonaws.services.elasticloadbalancing.model.RegisterInstancesWithLoadBalancerRequest;
 import com.google.common.base.Optional;
 import com.google.inject.Inject;
@@ -42,22 +44,44 @@ public class AgentAddedElbListener extends AbstractAgentWatchListener {
 
   @Override
   public void agentUpdated(BaragonAgentMetadata agent, Optional<BaragonGroup> group, String groupName) {
+    try {
+      if (shouldAddInstance(agent, group, groupName)) {
+        LOG.debug(String.format("Trying to register agent %s with ELB", agent.getAgentId()));
+        for (String elbName : group.get().getSources()) {
+          Instance instance = new Instance(agent.getInstanceId().get());
+          RegisterInstancesWithLoadBalancerRequest request = new RegisterInstancesWithLoadBalancerRequest(elbName, Arrays.asList(instance));
+          try {
+            elbClient.registerInstancesWithLoadBalancer(request);
+            LOG.info(String.format("Registered instances %s with ELB %s", request.getInstances(), request.getLoadBalancerName()));
+          } catch (AmazonClientException e) {
+            LOG.error("Could not register %s with elb %s due to error %s", request.getInstances(), request.getLoadBalancerName(), e);
+          }
+        }
+      }
+    } catch (Exception e) {
+      LOG.error(String.format("Could not check instance elb registration due to error %s", e), e);
+    }
+  }
+
+  private LoadBalancerDescription elbByName(String elbName) {
+    DescribeLoadBalancersRequest request = new DescribeLoadBalancersRequest(Arrays.asList(elbName));
+    return elbClient.describeLoadBalancers(request).getLoadBalancerDescriptions().get(0);
+  }
+
+  private boolean shouldAddInstance(BaragonAgentMetadata agent, Optional<BaragonGroup> group, String groupName) {
     if (group.isPresent()) {
       if (!group.get().getSources().isEmpty()) {
         if (agent.getInstanceId().isPresent()) {
-          LOG.debug(String.format("Trying to register agent %s with ELB", agent.getAgentId()));
           for (String elbName : group.get().getSources()) {
             Instance instance = new Instance(agent.getInstanceId().get());
-            RegisterInstancesWithLoadBalancerRequest request = new RegisterInstancesWithLoadBalancerRequest(elbName, Arrays.asList(instance));
-            try {
-              elbClient.registerInstancesWithLoadBalancer(request);
-              LOG.info(String.format("Registered instances %s with ELB %s", request.getInstances(), request.getLoadBalancerName()));
-            } catch (AmazonClientException e) {
-              LOG.error("Could not register %s with elb %s due to error %s", request.getInstances(), request.getLoadBalancerName(), e);
+            if (!elbByName(elbName).getInstances().contains(instance)) {
+              return true;
+            } else {
+              LOG.debug(String.format("Agent %s already registered with ELB %s", agent.getAgentId(), elbName));
             }
           }
         } else {
-         LOG.debug(String.format("No instance id for agent %s, can't add to ELB", agent.getAgentId()));
+          LOG.debug(String.format("No instance id for agent %s, can't add to ELB", agent.getAgentId()));
         }
       } else {
         LOG.debug(String.format("No traffic sources for group %s, not adding agent %s to an ELB", group.get().getName(), agent.getAgentId()));
@@ -65,6 +89,7 @@ public class AgentAddedElbListener extends AbstractAgentWatchListener {
     } else {
       LOG.debug(String.format("Group %s not found for agent %s", groupName, agent.getAgentId()));
     }
+    return false;
   }
 
   @Override
