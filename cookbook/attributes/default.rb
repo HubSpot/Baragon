859368bff3a60307eb5947bfb1c65b712663eee8
Jeff Byrnes <jeff@evertrue.com>
2015-08-31 14:22:09 -0400

    Point to current baragon-cookbook repo

--- cookbook/attributes/default.rb
+++
@@ -1,6 +0,0 @@
-default[:baragon][:version] = '0.1.7'
-default[:baragon][:zk_namespace] = 'baragon'
-default[:baragon][:zk_port] = 2181
-default[:baragon][:user] = 'baragon'
-default[:baragon][:git_ref] = 'Baragon-0.1.7'
-default[:baragon][:install_type] = 'package'
