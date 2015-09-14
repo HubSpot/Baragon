0915e2b2552e81867ab777f84e407066563e4aed
Stephen Salinas <ssalinas@hubspot.com>
2015-09-14 09:15:54 -0400

    override equals/toString/hashCode

--- BaragonCore/src/main/java/com/hubspot/baragon/models/BaragonRequestKey.java
+++ BaragonCore/src/main/java/com/hubspot/baragon/models/BaragonRequestKey.java
@@ -21,4 +21,36 @@ public class BaragonRequestKey implements Comparable<BaragonRequestKey> {
   public int compareTo(BaragonRequestKey o) {
     return Long.compare(updatedAt, o.updatedAt);
   }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    BaragonRequestKey that = (BaragonRequestKey) o;
+
+    if (updatedAt != that.updatedAt) {
+      return false;
+    }
+    if (requestId != null ? !requestId.equals(that.requestId) : that.requestId != null) {
+      return false;
+    }
+
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = requestId != null ? requestId.hashCode() : 0;
+    result = 31 * result + (int) (updatedAt ^ (updatedAt >>> 32));
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "BaragonRequestKey{" +
+      "requestId='" + requestId + '\'' +
+      ", updatedAt=" + updatedAt +
+      '}';
+  }
 }
