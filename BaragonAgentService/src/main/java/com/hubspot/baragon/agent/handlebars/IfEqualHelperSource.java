d0a6756084e69f1548a92ee1fe8dc0f178bbfade
Stephen Salinas <ssalinas@hubspot.com>
2016-02-01 16:37:59 -0500

    handle null in ifEqual helper

--- BaragonAgentService/src/main/java/com/hubspot/baragon/agent/handlebars/IfEqualHelperSource.java
+++ BaragonAgentService/src/main/java/com/hubspot/baragon/agent/handlebars/IfEqualHelperSource.java
@@ -6,7 +6,7 @@ import com.github.jknack.handlebars.Options;
 
 public class IfEqualHelperSource {
   public static CharSequence ifEqual(String v1, String v2, Options options) throws IOException {
-    if (v1.equals(v2)) {
+    if (v1 == null ? v2 == null : v1.equals(v2)) {
       return options.fn();
     } else {
       return options.inverse();
