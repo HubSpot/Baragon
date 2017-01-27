fdf8e1ce7684c084e2b9f903515f28f2cb25819d
Peter Teixeira <pteixeira@hubspot.com>
2017-01-27 12:09:44 -0500

    Add ability to merge config files
    Adds a config provider that allows multiple config providers to be
    joined. This allows as single configuration POJO to be stored in
    seperate files on disk, eg for versioning part of it while keeping the
    other secret.

---
+++ BaragonService/src/test/java/com/hubspot/baragon/service/config/MergingConfigProviderTest.java
@@ -0,0 +1,113 @@
+package com.hubspot.baragon.service.config;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.jukito.JukitoModule;
+import org.jukito.JukitoRunner;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.google.inject.Inject;
+
+import io.dropwizard.configuration.ConfigurationSourceProvider;
+import io.dropwizard.jackson.Jackson;
+
+@RunWith(JukitoRunner.class)
+public class MergingConfigProviderTest {
+
+  @Rule
+  public ExpectedException expectedException = ExpectedException.none();
+
+  private static final String DEFAULT_PATH = "/configs/default.yaml";
+  private static final String OVERRIDE_PATH = "/configs/override.yaml";
+  private static final String JUST_A_STRING_PATH = "/configs/just_a_string.yaml";
+  private static final String DOESNT_EXIST_PATH = "/configs/doesnt_exist.yaml";
+
+  private static final YAMLFactory YAML_FACTORY = new YAMLFactory();
+
+  public static class Module extends JukitoModule {
+    @Override
+    protected void configureTest() {
+      ObjectMapper mapper = Jackson.newObjectMapper()
+          .setSerializationInclusion(Include.NON_NULL)
+          .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+      bind(ObjectMapper.class).toInstance(mapper);
+    }
+  }
+
+  @Inject
+  ObjectMapper objectMapper;
+
+  private ConfigurationSourceProvider buildConfigSourceProvider(String baseFileName) {
+    final Class<?> klass = this.getClass();
+
+    return new MergingConfigProvider(new ConfigurationSourceProvider() {
+      @Override
+      public InputStream open(String path) throws IOException {
+        InputStream inputStream = klass.getResourceAsStream(path);
+        if (inputStream == null) {
+          throw new FileNotFoundException(String.format("File %s not found in test resources directory", path));
+        } else {
+          return inputStream;
+        }
+      }
+    }, baseFileName, objectMapper, YAML_FACTORY);
+  }
+
+  @Test
+  public void itMergesNestedConfigsProperly() throws Exception {
+    InputStream mergedConfigStream = buildConfigSourceProvider(DEFAULT_PATH)
+        .open(OVERRIDE_PATH);
+    BaragonConfiguration mergedConfig = objectMapper.readValue(
+        YAML_FACTORY.createParser(mergedConfigStream),
+        BaragonConfiguration.class);
+
+    assertEquals("override-quorum", mergedConfig.getZooKeeperConfiguration().getQuorum());
+    assertEquals(100, (int) mergedConfig.getZooKeeperConfiguration().getSessionTimeoutMillis());
+    assertEquals("override-namespace", mergedConfig.getZooKeeperConfiguration().getZkNamespace());
+    assertEquals(100, (int) mergedConfig.getZooKeeperConfiguration().getConnectTimeoutMillis());
+    assertEquals(100, (int) mergedConfig.getZooKeeperConfiguration().getRetryBaseSleepTimeMilliseconds());
+    assertEquals(2, mergedConfig.getAgentMaxAttempts());
+    assertEquals("127.0.0.1", mergedConfig.getHostname());
+    assertEquals("master-auth-key", mergedConfig.getMasterAuthKey());
+  }
+
+  @Test
+  public void itThrowsExnOnNonObjectOverride() throws Exception {
+    expectedException.expect(IllegalArgumentException.class);
+    buildConfigSourceProvider(DEFAULT_PATH)
+        .open(JUST_A_STRING_PATH);
+  }
+
+  @Test
+  public void itThrowsExnOnNonObjectDefault() throws Exception {
+    expectedException.expect(IllegalArgumentException.class);
+    buildConfigSourceProvider(JUST_A_STRING_PATH)
+        .open(OVERRIDE_PATH);
+  }
+
+  @Test
+  public void itThrowsExnOnNotFoundOverridePath() throws Exception {
+    expectedException.expect(FileNotFoundException.class);
+    buildConfigSourceProvider(DEFAULT_PATH)
+        .open(DOESNT_EXIST_PATH);
+  }
+
+  @Test
+  public void itThrowsExnOnNotFoundDefaultPath() throws Exception {
+    expectedException.expect(FileNotFoundException.class);
+    buildConfigSourceProvider(DOESNT_EXIST_PATH)
+        .open(OVERRIDE_PATH);
+  }
+}
