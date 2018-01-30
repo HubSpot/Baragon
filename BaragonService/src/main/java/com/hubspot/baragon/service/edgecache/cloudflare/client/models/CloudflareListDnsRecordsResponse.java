3c7c869aad3d83e57ae89d218413afd9ea0afd96
Stephen Salinas <ssalinas@hubspot.com>
2018-01-30 11:57:02 -0500

    Dedupe dns records for zone

--- BaragonService/src/main/java/com/hubspot/baragon/service/edgecache/cloudflare/client/models/CloudflareListDnsRecordsResponse.java
+++ BaragonService/src/main/java/com/hubspot/baragon/service/edgecache/cloudflare/client/models/CloudflareListDnsRecordsResponse.java
@@ -1,20 +1,21 @@
 package com.hubspot.baragon.service.edgecache.cloudflare.client.models;
 
 import java.util.List;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties( ignoreUnknown = true )
-public class CloudflareListDnsRecordsResponse extends CloudflareResponse<List<CloudflareDnsRecord>> {
+public class CloudflareListDnsRecordsResponse extends CloudflareResponse<Set<CloudflareDnsRecord>> {
 
   @JsonCreator
   public CloudflareListDnsRecordsResponse(@JsonProperty("success") Boolean success,
                                           @JsonProperty("errors") List<CloudflareError> errors,
                                           @JsonProperty("messages") List<String> messages,
                                           @JsonProperty("result_info") CloudflareResultInfo resultInfo,
-                                          @JsonProperty("result") List<CloudflareDnsRecord> result) {
+                                          @JsonProperty("result") Set<CloudflareDnsRecord> result) {
     super(success, errors, messages, resultInfo, result);
   }
 }
