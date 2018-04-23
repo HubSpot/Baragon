27b9dab5a58cd802888afc19ef52620ea2bcc185
Stephen Salinas <ssalinas@hubspot.com>
2018-04-23 09:59:28 -0400

    use Ning, deps don't work with apache client

--- BaragonAgentService/src/main/java/com/hubspot/baragon/agent/BaragonAgentServiceModule.java
+++ BaragonAgentService/src/main/java/com/hubspot/baragon/agent/BaragonAgentServiceModule.java
@@ -62,9 +62,8 @@ import com.hubspot.baragon.models.BaragonAgentMetadata;
 import com.hubspot.baragon.models.BaragonAgentState;
 import com.hubspot.baragon.utils.JavaUtils;
 import com.hubspot.dropwizard.guicier.DropwizardAwareModule;
-import com.hubspot.horizon.HttpClient;
 import com.hubspot.horizon.HttpConfig;
-import com.hubspot.horizon.apache.ApacheHttpClient;
+import com.hubspot.horizon.ning.NingHttpClient;
 
 import io.dropwizard.jetty.HttpConnectorFactory;
 import io.dropwizard.server.SimpleServerFactory;
@@ -265,7 +264,7 @@ public class BaragonAgentServiceModule extends DropwizardAwareModule<BaragonAgen
   @Provides
   @Singleton
   @Named(BARAGON_AGENT_HTTP_CLIENT)
-  public HttpClient providesApacheHttpClient(HttpClientConfiguration config, ObjectMapper objectMapper) {
+  public NingHttpClient providesApacheHttpClient(HttpClientConfiguration config, ObjectMapper objectMapper) {
     HttpConfig.Builder configBuilder = HttpConfig.newBuilder()
       .setRequestTimeoutSeconds(config.getRequestTimeoutInMs() / 1000)
       .setUserAgent(config.getUserAgent())
@@ -274,7 +273,7 @@ public class BaragonAgentServiceModule extends DropwizardAwareModule<BaragonAgen
       .setMaxRetries(config.getMaxRequestRetry())
       .setObjectMapper(objectMapper);
 
-    return new ApacheHttpClient(configBuilder.build());
+    return new NingHttpClient(configBuilder.build());
   }
 
   @Singleton
