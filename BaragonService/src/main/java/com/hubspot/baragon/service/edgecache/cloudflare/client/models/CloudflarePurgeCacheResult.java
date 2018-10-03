081718a5e97d37e3ea913fa8d59d36bb817e75aa
Gowtam Lal <glal@hubspot.com>
2018-10-03 16:34:42 -0400

    Checkstyle.

--- BaragonService/src/main/java/com/hubspot/baragon/service/edgecache/cloudflare/client/models/CloudflarePurgeCacheResult.java
+++ BaragonService/src/main/java/com/hubspot/baragon/service/edgecache/cloudflare/client/models/CloudflarePurgeCacheResult.java
@@ -19,8 +19,12 @@ public class CloudflarePurgeCacheResult {
 
   @Override
   public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
 
     CloudflarePurgeCacheResult that = (CloudflarePurgeCacheResult) o;
 
