27b401f8dcf66fe0d673eedac6e0b2f54e1aa706
Sharon Jerop Kipruto <sjeropkipruto@hubspot.com>
2018-10-17 16:14:58 -0400

    testing current set up as it is

--- BaragonAgentService/src/test/java/com/hubspot/baragon/agent/WeightingTest.java
+++ BaragonAgentService/src/test/java/com/hubspot/baragon/agent/WeightingTest.java
@@ -8,7 +8,6 @@ import org.jukito.JukitoRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import com.github.jknack.handlebars.Handlebars;
 import com.google.common.base.Optional;
 import com.hubspot.baragon.agent.config.BaragonAgentConfiguration;
 import com.hubspot.baragon.agent.handlebars.PreferSameRackWeightingHelper;
@@ -20,31 +19,31 @@ import com.hubspot.baragon.models.UpstreamInfo;
 @RunWith(JukitoRunner.class)
 public class WeightingTest {
 
-  final String requestId = "test-126";
-  final String requestId1 = "test-127";
-  final String requestId2 = "test-128";
-  final UpstreamInfo currentUpstream = new UpstreamInfo("testhost:8080", Optional.of(requestId), Optional.<String>absent());
-  final UpstreamInfo testUpstream1 = new UpstreamInfo("testhost:8082", Optional.of(requestId1), Optional.<String>absent());
-  final UpstreamInfo testUpstream2 = new UpstreamInfo("testhost:8081", Optional.of(requestId2), Optional.<String>absent());
-  final List<UpstreamInfo> upstreams = Arrays.asList(testUpstream1, testUpstream2);
-  final Object[] params = new Object[1];
-  final Handlebars handlebars = new Handlebars();
-//  final Options options = new Options.Builder(handlebars, "PreferSameRankWeightingHelper",).build();
+  final UpstreamInfo currentUpstream = new UpstreamInfo("testhost:8080", Optional.of("test-126"), Optional.of("us-east-5"));
+
+  final UpstreamInfo testUpstream1 = new UpstreamInfo("testhost:8080", Optional.of("test-126"), Optional.of("us-east-1"));
+  final UpstreamInfo testUpstream2 = new UpstreamInfo("testhost:8080", Optional.of("test-126"), Optional.of("us-east-1"));
+  final UpstreamInfo testUpstream3 = new UpstreamInfo("testhost:8080", Optional.of("test-126"), Optional.of("us-east-2"));
+  final UpstreamInfo testUpstream4 = new UpstreamInfo("testhost:8080", Optional.of("test-126"), Optional.of("us-east-3"));
+  final UpstreamInfo testUpstream6 = new UpstreamInfo("testhost:8080", Optional.of("test-126"), Optional.of("us-east-3"));
+  final UpstreamInfo testUpstream5 = new UpstreamInfo("testhost:8080", Optional.of("test-126"), Optional.of("us-east-5"));
+  final List<UpstreamInfo> upstreams = Arrays.asList(testUpstream1, testUpstream2, testUpstream3, testUpstream4, testUpstream5, testUpstream6);
+
   private final BaragonAgentConfiguration configuration = new BaragonAgentConfiguration();
   public static final String AGENT_ID = "123.123.123.123:8080";
   public static final String BASE_URI = "http://123.123.123.123:8080/baragon-agent/v2";
   public static final String DOMAIN = "test.com";
-  final BaragonAgentMetadata agentMetadata = new BaragonAgentMetadata(BASE_URI, AGENT_ID, Optional.of(DOMAIN), new BaragonAgentEc2Metadata(Optional.absent(), Optional.absent(), Optional.absent(), Optional.absent(), Optional.absent()), Optional.absent(), Collections
+  final BaragonAgentMetadata agentMetadata = new BaragonAgentMetadata(BASE_URI, AGENT_ID, Optional.of(DOMAIN),
+      new BaragonAgentEc2Metadata(Optional.absent(), Optional.of("us-east-5"), Optional.absent(), Optional.absent(), Optional.absent()), Optional.absent(), Collections
       .emptyMap(), true);
-  final PreferSameRackWeightingHelper helper = new PreferSameRackWeightingHelper(configuration, agentMetadata);
 
+  final PreferSameRackWeightingHelper helper = new PreferSameRackWeightingHelper(configuration, agentMetadata);
 
 
   @Test
   public void simpleTest() {
-//    CharSequence result = helper.preferSameRackWeighting(upstreams, currentUpstream, options);
-    System.out.println("yes");
-
+    CharSequence result = helper.preferSameRackWeighting(upstreams, currentUpstream, null);
+    System.out.println("result: " + result.toString());
   }
 
 
