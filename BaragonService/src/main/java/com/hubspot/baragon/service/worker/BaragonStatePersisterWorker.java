0bd5de90d697ee2f1d4c97f879a3692efa96f57a
Sharon Jerop Kipruto <sjeropkipruto@hubspot.com>
2019-05-15 12:22:07 -0400

    persist

--- BaragonService/src/main/java/com/hubspot/baragon/service/worker/BaragonStatePersisterWorker.java
+++ BaragonService/src/main/java/com/hubspot/baragon/service/worker/BaragonStatePersisterWorker.java
@@ -1,22 +1,35 @@
 package com.hubspot.baragon.service.worker;
 
+import java.util.Collection;
 import java.util.ServiceLoader;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.hubspot.baragon.data.BaragonStateDatastore;
+import com.hubspot.baragon.models.BaragonServiceState;
 import com.hubspot.baragon.state.persister.BaragonStatePersister;
 
 public class BaragonStatePersisterWorker implements Runnable {
+  private static final Logger LOG = LoggerFactory.getLogger(BaragonStatePersisterWorker.class);
 
-  private ServiceLoader<BaragonStatePersister> loader;
+  private final ServiceLoader<BaragonStatePersister> loader;
+  private final BaragonStateDatastore stateDatastore;
 
-  private BaragonStatePersisterWorker() {
-    loader = ServiceLoader.load(BaragonStatePersister.class);
+  private BaragonStatePersisterWorker(BaragonStateDatastore stateDatastore) {
+    this.loader = ServiceLoader.load(BaragonStatePersister.class);
+    this.stateDatastore = stateDatastore;
   }
 
   @Override
   public void run() {
-    for (BaragonStatePersister baragonStatePersister: loader) {
-      //TODO: do the work
-    }
-  }
+    Collection<BaragonServiceState> state = stateDatastore.getGlobalState();
+    int version = stateDatastore.getStateVersion().or(-1);
 
+    LOG.info("Baragon version: {}", version);
+    for (BaragonStatePersister baragonStatePersister: loader) {
+      baragonStatePersister.persist(state, version);
+    }
+
+  }
 }
