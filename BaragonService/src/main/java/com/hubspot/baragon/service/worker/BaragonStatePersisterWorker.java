ad9b3db86a940018eab27940bae09489fde7958b
Sharon Jerop Kipruto <sjeropkipruto@hubspot.com>
2019-05-17 11:12:53 -0400

    handle when state version is absent

--- BaragonService/src/main/java/com/hubspot/baragon/service/worker/BaragonStatePersisterWorker.java
+++ BaragonService/src/main/java/com/hubspot/baragon/service/worker/BaragonStatePersisterWorker.java
@@ -6,8 +6,10 @@ import java.util.ServiceLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Optional;
 import com.hubspot.baragon.data.BaragonStateDatastore;
 import com.hubspot.baragon.models.BaragonServiceState;
+import com.hubspot.baragon.service.exceptions.BaragonExceptionNotifier;
 import com.hubspot.baragon.state.persister.BaragonStatePersister;
 
 public class BaragonStatePersisterWorker implements Runnable {
@@ -15,21 +17,30 @@ public class BaragonStatePersisterWorker implements Runnable {
 
   private final ServiceLoader<BaragonStatePersister> loader;
   private final BaragonStateDatastore stateDatastore;
+  private final BaragonExceptionNotifier exceptionNotifier;
 
-  private BaragonStatePersisterWorker(BaragonStateDatastore stateDatastore) {
+  private BaragonStatePersisterWorker(BaragonStateDatastore stateDatastore,
+                                      BaragonExceptionNotifier exceptionNotifier) {
     this.loader = ServiceLoader.load(BaragonStatePersister.class);
     this.stateDatastore = stateDatastore;
+    this.exceptionNotifier = exceptionNotifier;
   }
 
   @Override
   public void run() {
-    Collection<BaragonServiceState> state = stateDatastore.getGlobalState();
-    int version = stateDatastore.getStateVersion().or(-1);
-
-    LOG.info("Baragon version: {}", version);
-    for (BaragonStatePersister baragonStatePersister: loader) {
-      baragonStatePersister.persist(state, version);
+    try {
+      Collection<BaragonServiceState> state = stateDatastore.getGlobalState();
+      Optional<Integer> maybeVersion = stateDatastore.getStateVersion();
+      if (maybeVersion.isPresent()) {
+        int version = maybeVersion.get();
+        LOG.info("Baragon version: {}", version);
+        for (BaragonStatePersister baragonStatePersister: loader) {
+          baragonStatePersister.persist(state, version);
+        }
+      }
+    } catch (Exception e) {
+      LOG.error("Caught exception when persisting baragon service state", e);
+      exceptionNotifier.notify(e, null);
     }
-
   }
 }
