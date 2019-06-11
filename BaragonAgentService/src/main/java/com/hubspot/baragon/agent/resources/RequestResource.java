9025e44274a03deffe3280a44466784972d990b8
Gowtam Lal <glal@hubspot.com>
2019-06-11 17:57:34 -0400

    Keep track of changes to the upstreams for a service within a batch.

--- BaragonAgentService/src/main/java/com/hubspot/baragon/agent/resources/RequestResource.java
+++ BaragonAgentService/src/main/java/com/hubspot/baragon/agent/resources/RequestResource.java
@@ -1,5 +1,7 @@
 package com.hubspot.baragon.agent.resources;
 
+import java.util.Collections;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
@@ -12,6 +14,7 @@ import javax.ws.rs.core.Response;
 import com.google.common.base.Optional;
 import com.google.inject.Inject;
 import com.hubspot.baragon.agent.managers.AgentRequestManager;
+import com.hubspot.baragon.data.BaragonRequestDatastore;
 import com.hubspot.baragon.models.BaragonRequest;
 import com.hubspot.baragon.models.RequestAction;
 
@@ -21,26 +24,32 @@ import com.hubspot.baragon.models.RequestAction;
 @Consumes(MediaType.APPLICATION_JSON)
 public class RequestResource {
   private final AgentRequestManager agentRequestManager;
+  private final BaragonRequestDatastore requestDatastore;
 
   @Inject
-  public RequestResource(AgentRequestManager agentRequestManager) {
+  public RequestResource(AgentRequestManager agentRequestManager, BaragonRequestDatastore requestDatastore) {
     this.agentRequestManager = agentRequestManager;
+    this.requestDatastore = requestDatastore;
   }
 
   @POST
   @Path("/literal")
   public Response applyLiteral(@PathParam("requestId") String requestId, BaragonRequest baragonRequest) throws InterruptedException {
-    return agentRequestManager.processRequest(requestId, RequestAction.UPDATE, baragonRequest, Optional.absent(), false, Optional.absent());
+    return agentRequestManager.processRequest(requestId, RequestAction.UPDATE, baragonRequest, Optional.absent(), Collections.emptyMap(), false, Optional.absent());
   }
 
   @POST
   public Response apply(@PathParam("requestId") String requestId) throws InterruptedException {
-    return agentRequestManager.processRequest(requestId, Optional.<RequestAction>absent(), false, Optional.<Integer>absent());
+    return agentRequestManager.processRequest(
+        requestId, requestDatastore.getRequest(requestId), Collections.emptyMap(),
+        Optional.<RequestAction>absent(), false, Optional.<Integer>absent()
+    );
   }
 
   @DELETE
   public Response revert(@PathParam("requestId") String requestId) throws InterruptedException {
-    return agentRequestManager.processRequest(requestId, Optional.of(RequestAction.REVERT), false, Optional.<Integer>absent());
+    return agentRequestManager.processRequest(requestId, requestDatastore.getRequest(requestId), Collections.emptyMap(),
+        Optional.of(RequestAction.REVERT), false, Optional.<Integer>absent());
   }
 
 }
