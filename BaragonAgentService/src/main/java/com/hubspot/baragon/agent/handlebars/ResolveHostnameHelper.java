ed05fa9b29c50ced44b708ffa24dbf427b8fc50c
Gowtam Lal <glal@hubspot.com>
2019-06-14 18:31:52 -0400

    Logging.

--- BaragonAgentService/src/main/java/com/hubspot/baragon/agent/handlebars/ResolveHostnameHelper.java
+++ BaragonAgentService/src/main/java/com/hubspot/baragon/agent/handlebars/ResolveHostnameHelper.java
@@ -1,11 +1,15 @@
 package com.hubspot.baragon.agent.handlebars;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.github.jknack.handlebars.Helper;
 import com.github.jknack.handlebars.Options;
 import com.hubspot.baragon.models.UpstreamInfo;
 import com.hubspot.baragon.utils.UpstreamResolver;
 
 public class ResolveHostnameHelper implements Helper<Object> {
+  private static final Logger LOG = LoggerFactory.getLogger(ResolveHostnameHelper.class);
 
   public static final String NAME = "resolveHostname";
 
@@ -19,11 +23,14 @@ public class ResolveHostnameHelper implements Helper<Object> {
   public CharSequence apply(Object upstream, Options options) {
     if (upstream instanceof String) {
       String address = ((String) upstream);
+      LOG.trace("Trying to resolve a String upstream of {}", address);
       return resolver.resolveUpstreamDNS(address).or(address);
     } else if (upstream instanceof UpstreamInfo) {
       UpstreamInfo upstreamInfo = ((UpstreamInfo) upstream);
+      LOG.trace("Trying to resolve an UpstreamInfo upstream of {}", upstreamInfo);
       return upstreamInfo.getResolvedUpstream().or(resolver.resolveUpstreamDNS(upstreamInfo.getUpstream())).or(upstreamInfo.getUpstream());
     } else {
+      LOG.error("resolveHostname called with invalid context {} and options {}", upstream, options);
       throw new IllegalArgumentException(String.format("Don't know how to process upstream %s", upstream.toString()));
     }
   }
