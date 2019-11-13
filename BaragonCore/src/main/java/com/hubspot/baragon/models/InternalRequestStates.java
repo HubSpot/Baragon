9bd5546ec79ba6b4f54052ba0817bc4639fbf9f9
Stephen Salinas <ssalinas@hubspot.com>
2019-11-13 14:39:08 -0500

    Do not process subsequent requests for a service when some are already in flight

--- BaragonCore/src/main/java/com/hubspot/baragon/models/InternalRequestStates.java
+++ BaragonCore/src/main/java/com/hubspot/baragon/models/InternalRequestStates.java
@@ -1,28 +1,34 @@
 package com.hubspot.baragon.models;
 
 public enum InternalRequestStates {
-  PENDING(false),
-  INVALID_REQUEST_NOOP(false),
-  SEND_APPLY_REQUESTS(true),
-  CHECK_APPLY_RESPONSES(false),
-  COMPLETED(false),
-  FAILED_SEND_REVERT_REQUESTS(true),
-  FAILED_CHECK_REVERT_RESPONSES(false),
-  FAILED_REVERTED(false),
-  FAILED_REVERT_FAILED(false),
-  CANCELLED_SEND_REVERT_REQUESTS(true),
-  CANCELLED_CHECK_REVERT_RESPONSES(false),
-  CANCELLED(false),
-  FAILED_CANCEL_FAILED(false),
-  COMPLETED_POST_APPLY_FAILED(false),;
+  PENDING(false, false),
+  INVALID_REQUEST_NOOP(false, false),
+  SEND_APPLY_REQUESTS(true, false),
+  CHECK_APPLY_RESPONSES(false, true),
+  COMPLETED(false, false),
+  FAILED_SEND_REVERT_REQUESTS(true, false),
+  FAILED_CHECK_REVERT_RESPONSES(false, true),
+  FAILED_REVERTED(false, false),
+  FAILED_REVERT_FAILED(false, false),
+  CANCELLED_SEND_REVERT_REQUESTS(true, false),
+  CANCELLED_CHECK_REVERT_RESPONSES(false, true),
+  CANCELLED(false, false),
+  FAILED_CANCEL_FAILED(false, false),
+  COMPLETED_POST_APPLY_FAILED(false, false),;
 
   private final boolean requireAgentRequest;
+  private final boolean inFlight;
 
-  InternalRequestStates(boolean requireAgentRequest) {
+  InternalRequestStates(boolean requireAgentRequest, boolean inFlight) {
      this.requireAgentRequest = requireAgentRequest;
+     this.inFlight = inFlight;
   }
 
   public boolean isRequireAgentRequest() {
     return requireAgentRequest;
+  }
+
+  public boolean isInFlight() {
+    return inFlight;
   }
 }
